607c4c1b6a4875ac77c2af84ea44c0786510d05a
Philip Nguyen <supertaru@gmail.com>
2014-09-16 16:40:25 -0700

    Welcome page

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 describe "Can acces home page", :capybara do
   it "should have content" do
     visit root_path
-    page.must_have_content "Welcome#index"
+    page.must_have_content "Welcome"
   end
 end
